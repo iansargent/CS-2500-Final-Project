0c8102c86b5bb64fdeca415fa482476161d571cf
AtticusTarleton <atarleto@uvm.edu>
2025-04-29 14:26:44 -0400

    MAde the main FUNCtioN!!

---
+++ main.py
@@ -0,0 +1,62 @@
+## This is the file for using the database
+
+# Importing necessary packages
+import sqlite3
+import csv
+import math
+
+##adding records to superintendent table
+con = sqlite3.connect('private_schools.db', isolation_level=None)
+cur = con.cursor()
+
+#table is string, column is string
+def mean(table, column):
+    count_query = f"select COUNT({column}) from {table}"
+    sum_query = f"select sum({column}) from {table}"
+    count = cur.execute(count_query).fetchall()[0][0]
+    sum = cur.execute(sum_query).fetchall()[0][0]
+    avg = sum/count
+    return avg
+
+def standard_deviation(table, column):
+    mean_value = mean(table, column)
+    get_vals_query = f"select {column} from {table}"
+    values = cur.execute(get_vals_query).fetchall()
+    count_query = f"select COUNT({column}) from {table}"
+    count = cur.execute(count_query).fetchall()[0][0]
+    running_variance = 0
+    for i in values:
+        val = i[0]
+        deviation = (val - mean_value)**2
+        running_variance += deviation
+    running_variance = running_variance/count
+    standard_deviation_total = math.sqrt(running_variance)
+    return  standard_deviation_total
+
+def min(table, column):
+    get_vals_query = f"select {column} from {table} order by {column} asc"
+    vals = cur.execute(get_vals_query). fetchall()[0][0]
+    return vals
+def max(table, column):
+    get_vals_query = f"select {column} from {table} order by {column} desc"
+    vals = cur.execute(get_vals_query). fetchall()[0][0]
+    return vals
+def median(table, column):
+    count_query = f"select COUNT({column}) from {table}"
+    count = cur.execute(count_query).fetchall()[0][0]
+    get_vals_query = f"select {column} from {table} order by {column} desc"
+    vals = cur.execute(get_vals_query). fetchall()
+    if count % 2 == 0:
+         index = count//2
+         index_1 = index
+         index_2 = index + 1
+         return (vals[index_1][0]+vals[index_2][0])/2
+
+    else:
+        index = count/2 + .5
+        return vals[index][0]
+
+print(min("admissions", "num_applicants"))
+print(median("admissions", "num_applicants"))
+print(mean("admissions", "num_applicants"))
+
