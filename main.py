c05ccaf3767aaee0053705b863a11a3690cf12f0
Isarge05 <iansarge05@gmail.com>
2025-05-02 17:24:08 -0400

    oiewf

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ list = []
 
 # Table is string, column is string
 def mean(table, column):
-    count_query = f"select COUNT({column}) from {table}"
-    sum_query = f"select sum({column}) from {table}"
+    count_query = f"SELECT COUNT({column}) from {table}"
+    sum_query = f"SELECT sum({column}) from {table}"
     count = cur.execute(count_query).fetchall()[0][0]
     sum = cur.execute(sum_query).fetchall()[0][0]
     avg = sum/count
@@ -34,9 +34,9 @@ def mean(table, column):
 # Standard deviation function
 def standard_deviation(table, column):
     mean_value = mean(table, column)
-    get_vals_query = f"select {column} from {table}"
+    get_vals_query = f"SELECT {column} FROM {table}"
     values = cur.execute(get_vals_query).fetchall()
-    count_query = f"select COUNT({column}) from {table}"
+    count_query = f"SELECT COUNT({column}) FROM {table}"
     count = cur.execute(count_query).fetchall()[0][0]
     running_variance = 0
     for i in values:
@@ -49,21 +49,21 @@ def standard_deviation(table, column):
 
 # Minimum and Maximum functions
 def min(table, column):
-    get_vals_query = f"select {column} from {table} order by {column} asc"
+    get_vals_query = f"SELECT {column} FROM {table} ORDER BY {column} ASC"
     vals = cur.execute(get_vals_query).fetchall()[0][0]
     return vals
 
 
 def max(table, column):
-    get_vals_query = f"select {column} from {table} order by {column} desc"
+    get_vals_query = f"SELECT {column} FROM {table} ORDER BY {column} DESC"
     vals = cur.execute(get_vals_query).fetchall()[0][0]
     return vals
 
 # Median function
 def median(table, column):
-    count_query = f"select COUNT({column}) from {table}"
+    count_query = f"SELECT COUNT({column}) FROM {table}"
     count = cur.execute(count_query).fetchall()[0][0]
-    get_vals_query = f"select {column} from {table} order by {column} desc"
+    get_vals_query = f"SELECT {column} FROM {table} ORDER BY {column} DESC"
     vals = cur.execute(get_vals_query).fetchall()
     if count % 2 == 0:
          index = count//2
@@ -291,8 +291,7 @@ def statistical_summary():
         print(column)
     
     # User input for the column of interest
-    print("\nChoose a column to analyze:")
-    column = input("\nEnter the column name: ")
+    column = input("\nEnter the column you want to analyze: ")
     # Get column type for tailored statistics
     col_type = get_column_type(table, column)
     
